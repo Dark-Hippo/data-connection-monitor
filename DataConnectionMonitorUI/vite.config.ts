83323d8da515e07d307e4101a40b3f15e4be182f
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-01-30 16:33:35 +0000

    Add dockerfile to run in preview mode

--- DataConnectionMonitorUI/vite.config.ts
+++ DataConnectionMonitorUI/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  const port = Number(env.UI_PORT) || 8080;
+  return {
   plugins: [react()],
+  preview: {
+    port: port,
+    strictPort: true,
+  },
+  server: {
+    port: port,
+    strictPort: true,
+    host: true,
+  }}
 })
