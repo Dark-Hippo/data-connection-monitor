2d4ba15d3f2324b9d8e50816f9bcdb5c9c926993
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-02-14 16:31:44 +0000

    Add backarrow component

---
+++ DataConnectionMonitorUI/src/components/BackArrow.tsx
@@ -0,0 +1,9 @@
+import { ArrowLeft } from 'react-bootstrap-icons';
+
+export const BackArrow = () => {
+  return (
+    <div>
+      <ArrowLeft style={{ width: "1em", height: "1em" }} />
+    </div>
+  )
+}
