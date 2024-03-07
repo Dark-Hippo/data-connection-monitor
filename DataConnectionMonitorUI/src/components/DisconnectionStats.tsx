7a04a1ada42e835a7da429c1fe5a56d529ae918a
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-03-07 14:58:54 +0000

    Formatting and styling for stats page

--- DataConnectionMonitorUI/src/components/DisconnectionStats.tsx
+++ DataConnectionMonitorUI/src/components/DisconnectionStats.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from "react";
 
+import './DisconnectionStats.css';
+
 type DisconnectionStatsProps = {
   totalDisconnections: number;
   totalDowntime: number;
@@ -7,15 +9,32 @@ type DisconnectionStatsProps = {
 }
 
 export const DisconnectionStats = (props: DisconnectionStatsProps): ReactElement => {
-  const { totalDisconnections, totalDowntime, longestDisconnection: longestDowntime } = props;
+  const { totalDisconnections, totalDowntime, longestDisconnection } = props;
+
+  const convertToDowntime = (time: number): string => {
+    const timestring = time.toString();
+    const hours = timestring.slice(0, 2);
+    const minutes = timestring.slice(3, 5);
+    const seconds = timestring.slice(6, 8);
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
+
+  const longestDowntime = convertToDowntime(longestDisconnection);
 
   return (
-    <>
-      <ul>
-        <li>{totalDisconnections} total disconnections</li>
-        <li>{totalDowntime} seconds of total downtime</li>
-        <li>Longest downtime of {longestDowntime}</li>
-      </ul>
-    </>
+    <div className="stats">
+      <div className="details total-disconnections">
+        <strong>{totalDisconnections}</strong><br />
+        <span className="stats-text">Disconnections</span>
+      </div>
+      <div className="details total-downtime">
+        <strong>{totalDowntime}</strong><br />
+        <span className="stats-text">Total downtime</span>
+      </div>
+      <div className="details longest-downtime">
+        <strong>{longestDowntime}</strong><br />
+        <span className="stats-text">Longest disconnection</span>
+      </div>
+    </div>
   )
 }
