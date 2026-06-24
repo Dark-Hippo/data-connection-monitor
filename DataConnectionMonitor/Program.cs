adda88570b2550d30f7cc55182c23d6c066aa745
Tom Odell <Dark-Hippo@users.noreply.github.com>
2026-06-24 18:11:47 +0100

    fix: update DotNetEnv loading method to use TraversePath

--- DataConnectionMonitor/Program.cs
+++ DataConnectionMonitor/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
-DotNetEnv.Env.NoClobber().Load();
+DotNetEnv.Env.TraversePath().NoClobber().Load();
 
 var builder = new ConfigurationBuilder()
   .SetBasePath(Directory.GetCurrentDirectory())
